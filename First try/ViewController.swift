48527434a8f1c619639ae926b5d7da9f5fefd90a
Mélina Vigliotta <Melina@MacBook-Air-de-Melina.local>
2017-03-04 22:10:46 +0100

    Initial Commit

---
+++ First try/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First try
+//
+//  Created by Mélina Vigliotta on 04.03.17.
+//  Copyright © 2017 Mélina Vigliotta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
